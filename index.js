03a64192d4d5ab5259dacce0317d7b1a168649d6
kaustubh285 <kaustubh285@gmail.com>
2023-11-09 18:52:29 +0000

    code clean up, better comments

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-const os = require("os");
-
 class RandomGenerator {
-  constructor(uniqueIntegers) {
-    this.uniqueIntegers = uniqueIntegers;
+  constructor(customString) {
+    /*
+    A class for generating random strings with customizable options. A predefined list of characters are used to generate a unique combination. Any custom strings will be saved as "customString"
+    */
+    // TODO: IMPLEMENT CUSTOM STRING REPEAT OR INCLUSION
+    this.customString = customString;
     this.customCharacterSetAlphaLower = "abcdefghijklmnopqrstuvwxyz";
     this.customCharacterSetAlphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     this.customCharacterSetNumeric = "0123456789";
 
     this.customCharacterSetSymbols = "!@#$%^&*()-_+=<>?";
     this.customCharacterSetSpecial = "[]{}|;:',./";
-    this.customCharacterSetSpace = " ";
 
     this.customCharacterSetPunctuation = ". , ; :";
     this.customCharacterSetMath = "=+-*/";
     this.selectedArrayHistory = [];
     this.vocabularyOptions = [];
+
+    this.currentLengthLimit = 13;
   }
 
   getRndInteger(min, max) {
+    /* 
+    Generates a random integer within a specified range.
+    Parameters:
+      - min: Minimum value of the range.
+      - max: Maximum value of the range.
+    Returns: Random integer within the specified range.
+
+    */
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
   generateNum(nChars, maxGroups) {
+    /* 
+        Generates a new random numeric string based on the specified req.
+        Parameters:
+          - nChars: Length of the string to be generated.
+          - maxGroups: Maximum number of groups to be generated.
+        Returns: Random numeric string with specified characteristics.
+    */
     const random = Math.floor(Math.random() * (20 - 10 + 1) + 10);
     let currentT = (Date.now() * random).toString();
 
@@ -38,24 +56,24 @@ class RandomGenerator {
     for (let i = 0; i < maxGroups; i++) {
       let temp = (parseInt(completeNumber) * preNumber1) / preNumber2;
       completeNumber = completeNumber + temp.toString().substring();
-      console.log("________________");
-      console.log(temp);
     }
     return completeNumber;
   }
 
   getCharFromString(value) {
+    /*
+    Retrieves a character from the vocabulary based on the provided value.
+    Parameters:
+      - value: Numeric value used to determine the character.
+    Returns: Character from the vocabulary based on the provided value.
+    */
     let arr_index = this.getRndInteger(0, this.vocabularyOptions.length);
     if (
       this.selectedArrayHistory[-1] == arr_index ||
       this.selectedArrayHistory[-2] == arr_index ||
       this.selectedArrayHistory[-3] == arr_index
     ) {
-      console.log("had to skip!!");
-      console.log(arr_index);
-      console.log("^ OLD -------- v NEW");
       arr_index = this.getRndInteger(0, 7);
-      console.log(arr_index);
     }
     this.selectedArrayHistory.push(arr_index);
     let selectedArray = this.vocabularyOptions[arr_index];
@@ -64,39 +82,51 @@ class RandomGenerator {
   }
 
   createVocabulary(vocabulary) {
+    /*
+    Creates the vocabulary based on the provided criteria.
+    Parameters:
+      - vocabulary: Array specifying the character groups to include in the vocabulary.
+    Returns: None
+    */
+
+    const addCharSet = (name, set) => {
+      if (vocabulary.includes(name)) {
+        this.vocabularyOptions.push(set);
+      }
+    };
+
     if (vocabulary.includes("all")) {
       this.vocabularyOptions.push(this.customCharacterSetAlphaLower);
       this.vocabularyOptions.push(this.customCharacterSetAlphaUpper);
       this.vocabularyOptions.push(this.customCharacterSetNumeric);
       return;
     }
-    if (vocabulary.includes("lowercase")) {
-      this.vocabularyOptions.push(this.customCharacterSetAlphaLower);
-    }
 
-    if (vocabulary.includes("uppercase")) {
-      this.vocabularyOptions.push(this.customCharacterSetAlphaUpper);
-    }
+    addCharSet("lowercase", this.customCharacterSetAlphaLower);
+    addCharSet("uppercase", this.customCharacterSetAlphaUpper);
+    addCharSet("numeric", this.customCharacterSetNumeric);
+    addCharSet("symbols", this.customCharacterSetSymbols);
+    addCharSet("specials", this.customCharacterSetSpecial);
 
-    if (vocabulary.includes("numeric")) {
-      this.vocabularyOptions.push(this.customCharacterSetNumeric);
-    }
-    if (vocabulary.includes("symbols")) {
-      this.vocabularyOptions.push(this.customCharacterSetSymbols);
-    }
-
-    if (vocabulary.includes("specials")) {
-      this.vocabularyOptions.push(this.customCharacterSetSpecial);
-    }
-
-    if (vocabulary.includes("spaces")) {
-      this.vocabularyOptions.push(this.customCharacterSetSpace);
-    }
+    // TODO:CHECK IF NECESSARY
+    // if (vocabulary.includes("spaces")) {
+    //   this.vocabularyOptions.push(this.customCharacterSetSpace);
+    // }
   }
 
-  generate(nChars = 13, vocabulary = ["lowercase", "numeric"]) {
-    let maxGroups = Math.floor(nChars / 13) + 1;
-
+  generate(
+    nChars = this.currentLengthLimit,
+    vocabulary = ["lowercase", "numeric"]
+  ) {
+    /* 
+        Generates a truly random string based on the time and random calculations with specified characteristics.
+        Parameters:
+        nChars: Length of the string to be generated.
+        vocabulary: Array specifying the character groups to include in the string.
+        valid values : ["all", "uppercase", "lowercase", "numeric", "symbols", "specials"]
+        Returns: Random string with specified characteristics
+    */
+    let maxGroups = Math.floor(nChars / this.currentLengthLimit) + 1;
     this.createVocabulary(vocabulary);
     let trulyRandomNumber = this.generateNum(nChars, maxGroups);
     let trulyRandomString = "";
@@ -111,4 +141,4 @@ class RandomGenerator {
 
 const myConst = new RandomGenerator();
 
-console.log(myConst.generate(20));
+console.log(myConst.generate(40));
