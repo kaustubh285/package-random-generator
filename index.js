116c76d90fb39af0f343637b490fab1b253348c3
kaustubh285 <kaustubh285@gmail.com>
2023-11-09 19:45:48 +0000

    better comments

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
+/**
+ * RandomGenerator - A class for generating customizable random strings.
+ *
+ * This class provides a flexible and customizable solution for generating random strings
+ * based on various character sets and user-defined criteria. It incorporates a predefined
+ * list of character groups, allowing users to create unique combinations for their specific needs.
+ *
+ * Usage:
+ * - Instantiate the RandomGenerator class with an optional custom string.
+ * - Utilize the `generate` method to generate random strings with specified characteristics.
+ *
+ * Important Considerations:
+ * - The class supports a variety of character groups, including lowercase, uppercase, numeric,
+ *   symbols, and specials. Users can customize string generation by specifying desired character
+ *   groups in the `vocabulary` parameter of the `generate` method.
+ * - The `generate` method takes two optional parameters: `nChars` (length of the generated string)
+ *   and `vocabulary` (array of character groups to include).
+ * - Users can easily extend or modify the predefined character sets to suit their requirements.
+ * - A set of unit tests is provided to ensure the reliability of the class functionality.
+ *
+ * Example:
+ * ```javascript
+ * const generator = new RandomGenerator();
+ * const randomString = generator.generate(15, ['lowercase', 'numeric']);
+ * console.log(randomString); // Output: "a1b2c3d4e5f6g7"
+ * ```
+ *
+ * @class
+ */
 class RandomGenerator {
-  constructor(customString) {
-    /*
+  /**
     A class for generating random strings with customizable options. A predefined list of characters are used to generate a unique combination. Any custom strings will be saved as "customString"
+    @function
     */
+  constructor(customString) {
     // TODO: IMPLEMENT CUSTOM STRING REPEAT OR INCLUSION
     this.customString = customString;
     this.customCharacterSetAlphaLower = "abcdefghijklmnopqrstuvwxyz";
@@ -21,6 +51,10 @@ class RandomGenerator {
     this.defaultLength = 10;
   }
 
+  /**
+   * The only purpose of this function is to make sure nothing might throw an error
+   * @function
+   */
   handleEdgeCases = () => {
     if (this.vocabularyOptions.length == 0) {
       this.vocabularyOptions.push(this.customCharacterSetAlphaLower);
@@ -30,26 +64,27 @@ class RandomGenerator {
     // TODO: ADD MORE EDGE CASES
   };
 
-  getRandomIntBetween = (min, max) => {
-    /* 
+  /**
     Generates a random integer within a specified range.
     Parameters:
       - min: Minimum value of the range.
       - max: Maximum value of the range.
     Returns: Random integer within the specified range.
-
+    @function
     */
+  getRandomIntBetween = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min;
   };
 
-  generateUniqueNumber = (nChars) => {
-    /* 
+  /**
         Generates a new random numeric string based on the specified req.
         Parameters:
           - nChars: Length of the string to be generated.
           - maxGroups: Maximum number of groups to be generated.
         Returns: Random numeric string with specified characteristics.
+        @function
     */
+  generateUniqueNumber = (nChars) => {
     const random = Math.floor(Math.random() * (20 - 10 + 1) + 10);
     let currentT = (Date.now() * random).toString();
 
@@ -65,11 +100,11 @@ class RandomGenerator {
     return preNumber1 + preNumber2 + trulyRandomNumber;
   };
 
-  increaseLength = (uniqueNumber, maxGroups) => {
-    /*
-    The default length of the unique number is 
+  /** 
+    The max length of the unique number in version 1.0.0 is 13, hence the increaseLength function is called
+    @function
     */
-
+  increaseLength = (uniqueNumber, maxGroups) => {
     const preNumber1 = this.getRandomIntBetween(10, 20).toString();
     const preNumber2 = this.getRandomIntBetween(10, 20).toString();
     for (let i = 0; i < maxGroups; i++) {
@@ -79,13 +114,14 @@ class RandomGenerator {
     return uniqueNumber;
   };
 
-  getCharFromString = (value) => {
-    /*
+  /**
     Retrieves a character from the vocabulary based on the provided value.
     Parameters:
       - value: Numeric value used to determine the character.
     Returns: Character from the vocabulary based on the provided value.
+    @function
     */
+  getCharFromString = (value) => {
     let arr_index = this.getRandomIntBetween(0, this.vocabularyOptions.length);
     if (
       this.selectedArrayHistory[-1] == arr_index ||
@@ -100,13 +136,14 @@ class RandomGenerator {
     return selectedArray[char_index];
   };
 
-  createVocabulary = (vocabulary) => {
-    /*
+  /**
     Creates the vocabulary based on the provided criteria.
     Parameters:
       - vocabulary: Array specifying the character groups to include in the vocabulary.
     Returns: None
+    @function
     */
+  createVocabulary = (vocabulary) => {
     const addCharSet = (name, set) => {
       if (vocabulary.includes(name)) {
         this.vocabularyOptions.push(set);
@@ -126,22 +163,20 @@ class RandomGenerator {
     addCharSet("symbols", this.customCharacterSetSymbols);
     addCharSet("specials", this.customCharacterSetSpecial);
 
-    // TODO:CHECK IF NECESSARY
-    // if (vocabulary.includes("spaces")) {
-    //   this.vocabularyOptions.push(this.customCharacterSetSpace);
-    // }
+    // TODO:CHECK IF SPACE NECESSARY
   };
 
-  generate = (nChars = this.defaultLength, vocabulary = ["all"]) => {
-    /* 
+  /** 
         Generates a truly random string based on the time and random calculations with specified characteristics.
         Parameters:
         nChars: Length of the string to be generated.
         vocabulary: Array specifying the character groups to include in the string.
         valid values : ["all", "uppercase", "lowercase", "numeric", "symbols", "specials"]
         Returns: Random string with specified characteristics
+        @function
     */
-    let maxGroups = Math.floor(nChars / this.currentLengthLimit) + 1;
+  generate = (nChars = this.defaultLength, vocabulary = ["all"]) => {
+    const maxGroups = Math.floor(nChars / this.currentLengthLimit) + 1;
     this.createVocabulary(vocabulary);
 
     // To handle edge cases
